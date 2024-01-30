181191262beb25d3bf5aab7660da11750a06f6c1
sanasharathchandra <93203011+sanasharathchandra@users.noreply.github.com>
2024-01-31 01:36:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+
+	const firstdate=new Date(date1);
+	const seconddate=new Date(date2);
+	if(firstdate.getTime()===seconddate.getTime()) return0;
+	const diffinmilliseconds=Math.abs(firstdate.getTime()-seconddate.getTime());
+	const DiffInDays=Math.ceil(diffinmilliseconds/(1000*60*60*24));
+	return DiffInDays
 };
 
 // Do not change the code below.
